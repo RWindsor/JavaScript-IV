de87e99bda8e2c4e4386cc80fede51cd95bbfaef
RWindsor <rachelwindsor@msn.com>
2019-05-09 14:58:52 -0700

    new commit

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,134 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor (attribute){
+        this.name = attribute.name;
+        this.age = attribute.age;
+        this.location = attribute.location;
+    }
+    speak (){
+        return `Hello my name is ${this.name}, I am from ${this.location}.`
+    }
+}
+
+class Instructor extends Person {
+    constructor (instrAtt){
+        super(instrAtt);
+        this.specialty = instrAtt.specialty;
+        this.favLanguage = instrAtt.favLanguage;
+        this.catchPhrase = instrAtt.catchPhrase;
+    }
+    demo (subject){
+        return `Today we are learning about ${subject}.`;
+    }
+    grade (subject){
+        return `${this.name} recieves a perfect score on ${subject}.`;
+    }
+}
+
+class Student extends Person {
+    constructor (studAtt){
+        super(studAtt);
+        this.previousBackground = studAtt.previousBackground;
+        this.className = studAtt.className;
+        this.favSubjects = studAtt.favSubjects;
+    }
+    listsSubjects (){
+        return this.favSubjects.map(function (list){
+            return list;
+        });
+    }
+    prAssignment(subject){
+        return `${this.name} has submitted a PR for ${subject}.`;
+    }
+    sprintChallenge (subject){
+        return `${this.name} has begun sprint challenge on ${subject}.`;
+    }
+}
+
+class ProjectManager extends Instructor {
+    constructor (pmAtt){
+        super (pmAtt);
+        this.gradClassName = pmAtt.gradClassName;
+        this.favInstructor = pmAtt.favInstructor;
+    }
+    standUp (channel){
+        return `${this.name} announces to ${channel}, @channel standy times!`;
+    }
+    debugsCode(Student, subject){
+        return `${this.name} debugs ${Student.name}'s code on ${subject}.`;
+    }
+}
+
+//Instructors
+
+const joey = new Instructor ({
+    name: 'Joey',
+    location: 'New York City',
+    age: 27,
+    favLanguage: 'Java',
+    specialty: 'Front-end',
+    catchPhrase: 'I need my Java in the morning'
+});
+//console.log (joey);
+
+const phoebe = new Instructor ({
+    name: 'Phoebe',
+    location: 'Austin',
+    age: 34,
+    favLanguage: 'Javascript',
+    specialty: 'Front-end Development',
+    catchPhrase: 'Stay calm and spellcheck your code' 
+});
+// console.log(phoebe);
+
+// Students
+
+const monica = new Student ({
+    name: 'Monica',
+    location: 'Los Angeles',
+    age: 22,
+    previousBackground: 'No coding background',
+    className: 'Web20',
+    favSubjects: ['HTML', 'CSS', 'Gaming']
+});
+// console.log(monica);
+
+const chandler = new Student ({
+    name: 'Chandler',
+    location: 'Seattle',
+    age: 43,
+    previousBackground: '7 years in IT',
+    className: 'Web20',
+    favSubjects: ['Crytocurrency', 'Back-end']
+})
+// console.log (chandler);
+
+// Project Managers
+
+const rachel = new ProjectManager ({
+    name: 'Rachel',
+    location: 'Orlando',
+    age: 29,
+    gradClassName: 'Web18',
+    favInstructor: 'Phoebe'
+});
+// console.log(rachel);
+
+const ross = new ProjectManager ({
+    name: 'Ross',
+    location: 'Los Angeles',
+    age: 25,
+    gradClassName: 'Web17',
+    favInstructor: 'Joey'
+});
+// console.log(ross);
+
+// Test methods
+console.log(ross.speak());
+console.log(joey.demo('Advanced Javascript'));
+console.log(phoebe.grade('Adavanced Javascript'));
+console.log(monica.listsSubjects());
+console.log(chandler.prAssignment('Advanced Javascript'));
+console.log(monica.sprintChallenge('Advanced Javascript'));
+console.log(rachel.standUp('Web20'));
+console.log(ross.debugsCode('Advanced Javscript'));
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -4,6 +4,7 @@
 
 //1. Copy and paste your code or the solution from yesterday
 
+
 // function color(color) {
 //     console.log(`the color is ${this}`);
 // }
@@ -33,23 +34,6 @@
 
 // introducePerson.call(willma);
 
-/*
-  Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
-
-  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.
-
-  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
-
-  Each constructor function has unique properties and methods that are defined in their block comments below:
-*/
-
-/*
-  === GameObject ===
-  * createdAt
-  * dimensions (These represent the character's size in the video game)
-  * destroy() // prototype method -> returns the string: 'Object was removed from the game.'
-*/
-
 // function GameObject(charAtt) {
 //     this.createdAt = charAtt.createdAt;
 //     this.dimensions = charAtt.dimensions;
@@ -59,13 +43,6 @@
 //     return `${this.name} was removed from the game.`;
 //   }
   
-//   /*
-//     === CharacterStats ===
-//     * healthPoints
-//     * name
-//     * takeDamage() // prototype method -> returns the string '<object name> took damage.'
-//     * should inherit destroy() from GameObject's prototype
-//   */
   
 //   function CharacterStats(charStats) {
 //     GameObject.call(this, charStats)
@@ -79,17 +56,7 @@
 //   CharacterStats.prototype.takeDamage = function () {
 //     return `${this.name} took damage.`
 //   }
-  
-//   /*
-//     === Humanoid (Having an appearance or character resembling that of a human.) ===
-//     * team
-//     * weapons
-//     * language
-//     * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-//     * should inherit destroy() from GameObject through CharacterStats
-//     * should inherit takeDamage() from CharacterStats
-//   */
-  
+
 //   function Humanoid(classAtt) {
 //     CharacterStats.call(this, classAtt)
   
@@ -168,6 +135,7 @@
   
 
 //  2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
+
 
 // function color(color) {
 //     console.log(`the color is ${this}`);
